c7cbfc98d8a50152bdbe0cd05a53e7e599bd393f
Baiphoo <danuporn.sit@gmail.com>
2026-03-25 10:21:15 +0700

    fix: add QueryClientProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// 1. นำเข้าเครื่องมือ React Query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// 2. สร้างตัวจัดการ
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <React.StrictMode>
+    {/* 3. เอา Provider มาครอบ App ของเราไว้ */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
